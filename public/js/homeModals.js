78006a8734af63a77f900389affd4ada20e45244
LoicLemeu <lemeu.loic@gamil.com>
2022-01-05 17:35:08 +0100

    starting CSS

--- public/js/homeModals.js
+++ public/js/homeModals.js
@@ -3,7 +3,6 @@ const hideButtons = document.querySelectorAll('.hideButton');
 
 for (let item of items){
     item.addEventListener('click', function(e){
-        console.log(e.target.className)
         if(e.target.className !== 'hideButton' ){
             item.lastChild.style.display= 'block';
         }
